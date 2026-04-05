685e6815402decda31ae956a6c2a159740f3f47c
tomilujan99 <tomilujan15@gmail.com>
2026-04-05 20:29:00 -0300

    Subo práctica unidad 1

---
+++ main.py
@@ -0,0 +1,9 @@
+
+print("Hola Mundo desde Main!")
+
+
+numero = int(input("ingrese un numero: "))
+numero2 = int(input("Ingrese otro numero: "))
+
+print(f"La suma de ambos numeros es: {numero+numero2}")
+
