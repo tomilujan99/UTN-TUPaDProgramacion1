5f98de629e909fa8891522c5b760b916af4fe80c
tomilujan99 <tomilujan15@gmail.com>
2026-04-05 20:45:00 -0300

    Se suma nueva funcionalidad a main

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 print("Hola Mundo desde Main!")
 
 
-numero = int(input("ingrese un numero: "))
+numero = int(input("Ingrese un numero: "))
 numero2 = int(input("Ingrese otro numero: "))
+numero3 = int(input("Ingrese un tercer numero: "))
 
-print(f"La suma de ambos numeros es: {numero+numero2}")
+print(f"La suma de ambos numeros es: {numero+numero2+numero3}")
 
